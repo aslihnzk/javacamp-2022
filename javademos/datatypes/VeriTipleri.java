2475f0cec7c20774b9d39ad9d3f283607496a45a
Aslıhan Özkan <114191738+aslihnzk@users.noreply.github.com>
2022-10-01 17:30:58 +0300

    Add files via upload

---
+++ javademos/datatypes/VeriTipleri.java
@@ -0,0 +1,10 @@
+public class VeriTipleri {
+    public static void main(String[] args) {
+        double sayi =12.5;
+        sayi=-129;
+        String sehir ="ANKARA";
+        char ='A';
+        Boolean dogruMu=false;
+
+    }
+}
