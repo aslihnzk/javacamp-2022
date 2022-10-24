1f9b621608021d58bb43fff774deb33e1719c6d0
Aslıhan Özkan <114191738+aslihnzk@users.noreply.github.com>
2022-10-25 00:00:48 +0300

    Add files via upload

---
+++ week-3/odev2/abstractDemo/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static void main(String[] args){
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.databaseManager = new MySqlDatabaseManager();
+        customerManager.setDatabaseManager();
+
+    }
+}
