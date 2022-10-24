5c4a81280a96675a0c77715ef6d40869edd5da21
Aslıhan Özkan <114191738+aslihnzk@users.noreply.github.com>
2022-10-25 00:18:16 +0300

    Add files via upload

---
+++ week3/homework1/homework2/abstractClasses/src/Main.java
@@ -0,0 +1,10 @@
+public abstract class Main {
+    public static void main(String[] args) {
+        WomanGameCalculator womanGameCalculator =new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+        GameCalculator gameCalculator =new WomanGameCalculator();
+
+
+    }
+}
---
+++ week3/homework1/homework2/staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,17 @@
+public class DatabaseHelper {
+    public static class Crud{// Create,read,update,delete
+        public static void Delete(){
+
+        }
+        public static Update(){
+
+        }
+        }
+        // inner class
+    public static class Connection {
+        public static void createConnection(){
+
+        }
+        }
+
+     }
