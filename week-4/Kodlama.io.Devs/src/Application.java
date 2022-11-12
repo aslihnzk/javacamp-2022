d8dde08e9ab5219b7c97f802b0018bb92f6e959e
Aslıhan Özkan <114191738+aslihnzk@users.noreply.github.com>
2022-11-13 00:06:17 +0300

    Delete week-4/Kodlama.io.Devs directory

--- week-4/Kodlama.io.Devs/src/Application.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Application {
-    public static void main(String[] args) {
-        Object SpringApplication = null;
-        SpringApplication.run(Application.class,args);
-    }
-
-}
--- week-4/Kodlama.io.Devs/src/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-public interface LanguageRepository {
-    public void add(Language language);
-    public void delete(int id);
-  public void update(int id,String name);
-    Language getById(int id);
-    List<Language>getAll();
-}
